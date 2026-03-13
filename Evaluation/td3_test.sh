d291e9ce8533d0299e2a2d65c68d0b1d2a502d09
CatherineMAbraham <catherinemabraham@gmail.com>
2026-03-13 14:45:39 +0000

    more mem

--- Evaluation/td3_test.sh
+++ Evaluation/td3_test.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=1            # 4 agents total
 #SBATCH --cpus-per-task=10      # 4 CPUs per agent
 #SBATCH --array=1-4
-#SBATCH --mem=8G              # 8GB RAM per agent
+#SBATCH --mem=20G              # 8GB RAM per agent
 #SBATCH --time=00:30:00
 
 module load Anaconda3/2024.02-1
