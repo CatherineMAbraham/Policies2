bb2a4fb7a20a2ec6ecce72df517f95e33753a008
CatherineMAbraham <catherinemabraham@gmail.com>
2026-03-17 09:34:08 +0000

    - actually on cpu

--- TD3/td3_cpu.sh
+++ TD3/td3_cpu.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #SBATCH --mail-user=cmabraham1@sheffield.ac.uk
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --partition=gpu
 #SBATCH --ntasks=1            # 4 agents total
 #SBATCH --cpus-per-task=1      # 4 CPUs per agent
 #SBATCH --mem=8G              # 8GB RAM per agent
-#SBATCH --array=1-8
+#SBATCH --array=1-2
 #SBATCH --time=28:00:00
 #SBATCH --output=out_%A_%a.out
 
