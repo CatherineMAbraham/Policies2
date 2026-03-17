4e84c8fe04af37f63adf2ee8619eb5912ccc2e79
CatherineMAbraham <catherinemabraham@gmail.com>
2026-03-17 09:29:14 +0000

    - run td3 test on cpu

--- TD3/td3_cpu.sh
+++ TD3/td3_cpu.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 #SBATCH --mail-user=cmabraham1@sheffield.ac.uk
 #SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --partition=gpu
 #SBATCH --ntasks=1            # 4 agents total
 #SBATCH --cpus-per-task=1      # 4 CPUs per agent
 #SBATCH --mem=8G              # 8GB RAM per agent
+#SBATCH --array=1-8
 #SBATCH --time=28:00:00
 #SBATCH --output=out_%A_%a.out
 
 module load Anaconda3/2024.02-1
 
 source activate softsurg
-
-SOFT=('spring')
-TISSUE=${SOFT[$SLURM_ARRAY_TASK_ID]}
+# Read the correct line from params_curr_compare.csv
+TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
+PARAM_LINE=$(sed -n "${TASK_ID}p" tests.csv)
+IFS=',' read -r TISSUE NUM_SPRINGS YM <<< "$PARAM_LINE"
+echo "Running test with: Tissue=$TISSUE, Num_Springs=$NUM_SPRINGS, Youngs_Modulus=$YM"
 # Run the script
-srun --export=ALL python td3.py --threshold_pos 0.001 --threshold_ori 5 --action_type euler --maxforce 3.5 --softtissue $TISSUE --ran $SLURM_ARRAY_TASK_ID
+#srun --export=ALL 
+python td3.py --threshold_pos 0.001 --threshold_ori 5 --action_type euler --maxforce 3.5 --softtissue "$TISSUE" --num_springs "$NUM_SPRINGS" --youngs_modulus "$YM" --contact_type 0 --ran $TASK_ID
