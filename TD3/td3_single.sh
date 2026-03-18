56ff18ed95dc3c190aab8c29af39af9e66ffb51b
CatherineMAbraham <catherinemabraham@gmail.com>
2026-03-18 13:45:02 +0000

    - test to check how it trains with no force consideration (set max force to extremely high value)

---
+++ TD3/td3_single.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --mail-user=cmabraham1@sheffield.ac.uk
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1            # 4 agents total
+#SBATCH --cpus-per-task=1      # 4 CPUs per agent
+#SBATCH --mem=8G              # 8GB RAM per agent
+#SBATCH --time=28:00:00
+#SBATCH --output=out_%A_%a.out
+
+
+module load Anaconda3/2024.02-1
+
+source activate softsurg
+# Read the correct line from params_curr_compare.csv
+
+# Run the script
+srun --export=ALL python td3.py --threshold_pos 0.001 --threshold_ori 5 --action_type euler --maxforce 50000000 --softtissue None --contact_type 0 --ran $TASK_ID --log 1
