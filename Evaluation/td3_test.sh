549400444cd624068fd5b4104d89f5783cec2e3d
CatherineMAbraham <catherinemabraham@gmail.com>
2026-03-20 15:42:03 +0000

    - changed model log - might as well run both

--- Evaluation/td3_test.sh
+++ Evaluation/td3_test.sh
@@ -6,6 +6,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1            # 4 agents total
 #SBATCH --cpus-per-task=10     # 4 CPUs per agent
+#SBATCH --array=1-2
 #SBATCH --mem=20G              # 8GB RAM per agent
 #SBATCH --time=02:00:00
 
@@ -15,7 +16,7 @@ source activate softsurg
 
 # Run the script
 TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
-PARAM_LINE=$(sed -n "${TASK_ID}p" /users/cop21cma/Policies2/TD3_Alg/model_log.csv)
+PARAM_LINE=$(sed -n "${TASK_ID}p" model_log.csv)
 IFS=',' read -r MODEL <<< "$PARAM_LINE"
 MODEL=${MODEL//\'/}
 echo "Testing model: $MODEL"
