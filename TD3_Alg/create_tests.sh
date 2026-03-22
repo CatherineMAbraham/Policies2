9cad101b610a522e6c1be52a77b578ca8423bed6
CatherineMAbraham <catherinemabraham@gmail.com>
2026-03-22 20:28:08 +0000

    -force tests to run

--- TD3_Alg/create_tests.sh
+++ TD3_Alg/create_tests.sh
@@ -4,16 +4,16 @@
 > tests.csv
 
 # Generate combinations of tissue option, number of springs
-for tissue in "spring"; do
-    if [ "$tissue" = "spring" ]; then
-        options=("1" "3")
+for tissue in "spring" None; do
+    if [ "$tissue" = "spring"  ]; then
+        options=("1e7" "5e6" "1e6")
     else
         options=("1")
     fi
 
-    for num in "${options[@]}"; do
-        for ym in "1e7" "5e7" "2e7" "1e8"; do
-            echo "$tissue,$num,$ym" >> tests.csv
+    for ym in "${options[@]}"; do
+        for max_force in "3" "3.5"; do
+            echo "$tissue,$ym,$max_force" >> tests.csv
         done
     done
 done
