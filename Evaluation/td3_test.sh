3718004fd783a8055a44a2ba715b4c375faadd61
CatherineMAbraham <catherinemabraham@gmail.com>
2026-03-23 17:25:17 +0000

    eval of 3 springs

--- Evaluation/td3_test.sh
+++ Evaluation/td3_test.sh
@@ -6,7 +6,6 @@
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1            # 4 agents total
 #SBATCH --cpus-per-task=10     # 4 CPUs per agent
-#SBATCH --array=1-2
 #SBATCH --mem=20G              # 8GB RAM per agent
 #SBATCH --time=02:00:00
 
@@ -15,16 +14,16 @@ module load Anaconda3/2024.02-1
 source activate softsurg
 
 # Run the script
-TASK_ID=${SLURM_ARRAY_TASK_ID:-2}
+TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
 PARAM_LINE=$(sed -n "${TASK_ID}p" model_log.csv)
 IFS=',' read -r MODEL <<< "$PARAM_LINE"
 MODEL=${MODEL//\'/}
-# Convert relative path to absolute
-# if [[ "$MODEL" == /* ]]; then
-#     FULL_MODEL_PATH=/users/cop21cma/Policies2/TD3_Alg"$MODEL"
-# else
-#     FULL_MODEL_PATH=$MODEL
-# fi
+Convert relative path to absolute
+if [[ "$MODEL" == /* ]]; then
+    FULL_MODEL_PATH=/users/cop21cma/Policies2/TD3_Alg"$MODEL"
+else
+    FULL_MODEL_PATH=$MODEL
+fi
 echo "Testing model: $MODEL"
 #srun --export=ALL 
-python env_test2.py --num_eps 1000 --n_envs 1 --model_path "$MODEL" --maxforce 4 --softtissue soft --youngs_modulus 1e6 --log 0
+python env_test2.py --num_eps 1000 --n_envs 10 --model_path "$MODEL" --maxforce 4 --softtissue soft --youngs_modulus 1e7 --log 1
