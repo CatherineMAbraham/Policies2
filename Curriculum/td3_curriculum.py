cca273456d7035c1dcd610f6250096fc5711bdf0
CatherineMAbraham <catherinemabraham@gmail.com>
2026-03-24 09:40:41 +0000

    imported log_Callback wrong

--- Curriculum/td3_curriculum.py
+++ Curriculum/td3_curriculum.py
@@ -21,7 +21,7 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
 
-from log_callback import log_callback
+import log_callback
 from success_callback import StopTrainingOnSuccessRate
 import os
 #repo_path = "/home/catherine/FractureGym/fracturesurgeryenv"
