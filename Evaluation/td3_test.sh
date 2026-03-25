e60fe53cbf2078da734b67d717b99bf2215c65b8
CatherineMAbraham <catherinemabraham@gmail.com>
2026-03-25 07:22:45 +0000

    increased envs

--- Evaluation/td3_test.sh
+++ Evaluation/td3_test.sh
@@ -5,10 +5,10 @@
 #SBATCH --qos=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1            # 4 agents total
-#SBATCH --cpus-per-task=1     # 4 CPUs per agent
+#SBATCH --cpus-per-task=5     # 4 CPUs per agent
 #SBATCH --mem=20G              # 8GB RAM per agent
 #SBATCH --array=1-10
-#SBATCH --time=03:00:00
+#SBATCH --time=05:00:00
 
 
 module load Anaconda3/2024.02-1
@@ -23,7 +23,8 @@ MODEL=${MODEL//\'/}
 if [[ "$MODEL" == /* ]]; then
     FULL_MODEL_PATH=/users/cop21cma/Policies2/TD3_Alg"$MODEL"
 else
-    FULL_MODEL_PATH=$MODEL
+    FULL_MODEL_PATH=/"$MODEL"
 fi
 echo "Testing model: $MODEL"
-srun --export=ALL python env_test2.py --num_eps 1000 --n_envs 1 --model_path "$FULL_MODEL_PATH" --maxforce 4 --softtissue soft --youngs_modulus 1e7 --log 1
+#srun --export=ALL 
+python env_test2.py --num_eps 1000 --n_envs 5 --model_path "$FULL_MODEL_PATH" --maxforce 4 --softtissue soft --youngs_modulus 1e7 --log 1
