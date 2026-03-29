436011dec6111fd7aaf272df63cbd5e6f9d21f28
CatherineMAbraham <catherinemabraham@gmail.com>
2026-03-29 20:49:48 +0100

    needed ntasks to be 10 not cpus

--- Sweep/sweep.sh
+++ Sweep/sweep.sh
@@ -4,8 +4,8 @@
 #SBATCH --partition=gpu
 #SBATCH --qos=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --ntasks=1            # 4 agents total
-#SBATCH --cpus-per-task=10      # 4 CPUs per agent
+#SBATCH --ntasks=10            # 4 agents total
+#SBATCH --cpus-per-task=1      # 4 CPUs per agent
 #SBATCH --mem=8G              # 8GB RAM per agent
 #SBATCH --time=40:00:00
 
