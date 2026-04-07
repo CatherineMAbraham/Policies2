308bd9d642422f3935a20c2fe4c822f0c64ddf79
CatherineMAbraham <catherinemabraham@gmail.com>
2026-04-07 14:10:16 +0100

    changed min evals to 1 and max no improvement to 10

--- TD3_Alg/td3.py
+++ TD3_Alg/td3.py
@@ -151,9 +151,9 @@ def train(threshold_pos=0.001,
     eval_env = VecNormalize(eval_env, norm_obs=True, norm_reward=False)
     log_callback1 = log_callback.CustomCallback()
     success_callback = StopTrainingOnSuccessRate(vec_env=eval_env, 
-                                                    max_no_improvement_evals=5, 
+                                                    max_no_improvement_evals=10, 
                                                     success_threshold=0.9,  
-                                                    min_evals=10, verbose=1, 
+                                                    min_evals=1, verbose=1, 
                                                     model_name = model_name,
                                                     model_save_path=f'./best_models/{ran}')
     eval_callback = EvalCallback(eval_env,  eval_freq=10000,
