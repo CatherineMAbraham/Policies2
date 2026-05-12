4a0cee8d9f7ff3d074c9dd2dc441ee58315e776b
CatherineMAbraham <catherinemabraham@gmail.com>
2026-05-12 19:50:21 +0100

    - new params to test

--- Chapter1/core_tests.sh
+++ Chapter1/core_tests.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1            # 4 agents total
 #SBATCH --cpus-per-task=2      # 4 CPUs per agent
 #SBATCH --mem=8G              # 8GB RAM per agent
-#SBATCH --time=20:00:00
+#SBATCH --time=96:00:00
 #SBATCH --array=1-8
 #SBATCH --output=out_%A_%a.out
 #SBATCH --error=err_%A_%a.err
@@ -14,8 +14,8 @@ source activate softsurg
 
 # Read the correct line from params_curr_compare.csv
 TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
-PARAM_LINE=$(sed -n "${TASK_ID}p" params.csv)
-#PARAM_LINE=$(sed -n "$((SLURM_ARRAY_TASK_ID))p" params.csv)
+PARAM_LINE=$(sed -n "${TASK_ID}p" params2.csv)
+#PARAM_LINE=$(sed -n "$((SLURM_ARRAY_TASK_ID))p" params2.csv)
 IFS=',' read -r THRESH_POS THRESH_ORI ACTION_TYPE<<< "$PARAM_LINE"
 
 # Run the script
